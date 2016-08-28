afa35c6761f9fddd6ff3e2f9d7afe1722dcc8e7a
Rupak Pandey <Rupak Pandey>
2016-08-28 14:49:10 +0545

    Initial Commit

---
+++ myblog/blogapp/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class BlogPost(models.Model):
+	author = models.CharField(max_length=25)
+	title =models.CharField(max_length=200)
+	content = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	publish = models.BooleanField(default=False)
+	
+	def __str__(self):
+		return self.title
+
